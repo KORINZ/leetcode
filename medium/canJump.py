82f6977670954f1f5ad788ce3ebb42a0bed35a78
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-02 01:58:17 +0900

    add canJump.py

---
+++ medium/canJump.py
@@ -0,0 +1,23 @@
+# You are given an integer array nums. You are initially positioned at the array's first index,
+# and each element in the array represents your maximum jump length at that position.
+# Return true if you can reach the last index, or false otherwise.
+
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def canJump(nums: List[int]) -> bool:
+        goal = len(nums) - 1
+        for i in range(len(nums) - 1, -1, -1):  # starting from goal to the beginning
+            if i + nums[i] >= goal:
+                goal = i
+
+        if goal == 0:  # check if goal can reach index 0
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    print(Solution().canJump(nums=[2, 5, 0, 0]))  #
