2c2f19689faae9d503c9d7301dd0a02cd88f0e92
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-01 20:43:20 +0900

    add isAnagram.py

---
+++ easy/isAnagram.py
@@ -0,0 +1,19 @@
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+class Solution:
+    @staticmethod
+    def isAnagram(s: str, t: str) -> bool:
+        s_letters = [letter for letter in s].sort()
+        t_letters = [letter for letter in t].sort()
+        if s_letters == t_letters:
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    print(Solution().isAnagram(s="listen", t="silent"))
+
+# Time Complexity: O(nlog(n))
+# Memory Complexity: O(n)
