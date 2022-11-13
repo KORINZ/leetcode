b381d8fb7f91ee2528152ced7eb9a87b7a8a1030
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-13 16:28:29 +0900

    add romanToInt.py

---
+++ easy/romanToInt.py
@@ -0,0 +1,48 @@
+"""
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two ones added together.
+12 is written as XII, which is simply X + II.
+The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII.
+Instead, the number four is written as IV. Because the one is before the five we subtract it making four.
+The same principle applies to the number nine, which is written as IX.
+There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+"""
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+
+        roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+        result = 0
+
+        for i in range(len(s)):
+            if len(s) - 1 > i and roman_dict[s[i]] < roman_dict[s[i + 1]]:
+                result -= roman_dict[s[i]]
+            else:
+                result += roman_dict[s[i]]
+        return result
+
+
+if __name__ == '__main__':
+    print(Solution().romanToInt('MCMXCIV'))  # 1994
+
+# Time Complexity: O(1)
+# Memory Complexity: O(1)
