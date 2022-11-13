f839d64b5b0819d605462891019940b13b5f72d1
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-13 22:15:36 +0900

    add empty minWindow.py

---
+++ hard/minWindow.py
@@ -0,0 +1,20 @@
+"""
+Given two strings s and t of lengths m and n respectively, return the minimum window
+substring of s such that every character in t (including duplicates) is included in the window.
+If there is no such substring, return the empty string "".
+The testcases will be generated such that the answer is unique.
+s and t consist of uppercase and lowercase English letters.
+"""
+
+
+class Solution:
+    @staticmethod
+    def minWindow(s: str, t: str) -> str:
+        pass
+
+
+if __name__ == '__main__':
+    print(Solution().minWindow(s="ADOBECODEBANC", t="ABC"))  # "BANC"
+
+# Time Complexity: O()
+# Memory Complexity: O()
