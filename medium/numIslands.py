a1925020c564868a63899276e38961396ff99950
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-18 01:17:36 +0900

    Add numIslands.py

---
+++ medium/numIslands.py
@@ -0,0 +1,42 @@
+"""
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+You may assume all four edges of the grid are all surrounded by water.
+"""
+from typing import List
+import collections
+
+
+class Solution:
+    @staticmethod
+    def numIslands(grid: List[List[str]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        islands = 0
+
+        def bfs(r, c):  # breadth first search
+            q = collections.deque()
+            visited.add(r, c)
+            q.append((r, c))
+
+            while q:
+                row, col = q.popleft()
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+                for dr, dc in directions:
+                    pass
+
+        for r in range(rows):
+            for c in range(cols):
+                bfs(r, c)
+                islands += 1
+        return islands
+
+
+if __name__ == '__main__':
+    print(Solution().numIslands(grid=[
+        ["1", "1", "0", "0", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "1", "0", "0"],
+        ["0", "0", "0", "1", "1"]
+    ]))  # 3
