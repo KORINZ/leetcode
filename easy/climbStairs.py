fc59fea32e38c2d6fa138cdfbee50197983df5be
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-22 15:52:19 +0900

    Add comments on output

--- easy/climbStairs.py
+++ easy/climbStairs.py
@@ -11,4 +11,11 @@ class Solution:
 
 
 if __name__ == '__main__':
-    print(Solution().climbStairs(n=3))
+    print(Solution().climbStairs(n=3))  # 3
+    # There are three ways to climb to the top.
+    # 1. 1 step + 1 step + 1 step
+    # 2. 1 step + 2 steps
+    # 3. 2 steps + 1 step
+
+# Time Complexity: O()
+# Memory Complexity: O()
