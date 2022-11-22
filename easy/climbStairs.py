bfd2af64ba658d09df0f3ebda5edd2d2f2830b01
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-22 15:49:55 +0900

    Add climbStairs.py

---
+++ easy/climbStairs.py
@@ -0,0 +1,14 @@
+"""
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+"""
+
+
+class Solution:
+    @staticmethod
+    def climbStairs(n: int) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    print(Solution().climbStairs(n=3))
