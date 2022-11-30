89ce4ca18f60e67f82a3264be9d8f6ff2966e610
コリン <111611023+KORINZ@users.noreply.github.com>
2022-12-01 02:22:16 +0900

    Update minWindow.py

--- hard/minWindow.py
+++ hard/minWindow.py
@@ -19,7 +19,7 @@ class Solution:
             countT[c] = 1 + countT.get(c, 0)
 
         have, need = 0, len(countT)
-        res, resLen = [-1, -1], float('inf')
+        res, res_len = [-1, -1], float('inf')
         l = 0
         for r in range(len(s)):
             c = s[r]
@@ -30,16 +30,16 @@ class Solution:
 
             while have == need:
                 # update result
-                if (r - l + 1) < resLen:
+                if (r - l + 1) < res_len:
                     res = [l, r]
-                    resLen = r - l + 1
+                    res_len = r - l + 1
                 # pop from the left of window
                 window[s[l]] -= 1
                 if s[l] in countT and window[s[l]] < countT[s[l]]:
                     have -= 1
                 l += 1
         l, r = res
-        return s[l : r + 1] if resLen != float('inf') else ""
+        return s[l : r + 1] if res_len != float('inf') else ""
 
 
 if __name__ == '__main__':
