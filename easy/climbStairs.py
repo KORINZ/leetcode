fdee138f8b251e5926254720bd3b2ae9b80d28b2
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-30 17:15:50 +0900

    Add constraint and NOQA to function name

--- easy/climbStairs.py
+++ easy/climbStairs.py
@@ -1,12 +1,15 @@
 """
 You are climbing a staircase. It takes n steps to reach the top.
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Constraint:
+1 <= n <= 45
 """
 
 
 class Solution:
     @staticmethod
-    def climbStairs(n: int) -> int:
+    def climbStairs(n: int) -> int:  # NOQA
         pass
 
 
