5414973f4e462721c6203a35c02a108559b1a008
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-02 18:14:56 +0900

    Update code format

--- hard/longestCommonSubsequence.py
+++ hard/longestCommonSubsequence.py
@@ -1,8 +1,19 @@
 """
 https://leetcode.com/problems/longest-common-subsequence/
-Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+Given two strings text1 and text2, return the length of their longest common subsequence.
+If there is no common subsequence, return 0.
 A subsequence of a string is a new string generated from the original string with 
 some characters (can be none) deleted without changing the relative order of the remaining characters.
 For example, "ace" is a subsequence of "abcde".
 A common subsequence of two strings is a subsequence that is common to both strings.
 """
+
+
+class Solution:
+    @staticmethod
+    def longestCommonSubsequence(text1: str, text2: str) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    pass
