bdea75bd45e215e0cb3fbb120ebd2ff7c6449142
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-07 21:28:05 +0900

    Add sortedSquares.py

---
+++ Data Structures and Algorithms/Arrays and strings/sortedSquares.py
@@ -0,0 +1,15 @@
+"""
+Given an integer array nums sorted in non-decreasing order,
+return an array of the squares of each number sorted in non-decreasing order.
+"""
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def sortedSquares(nums: List[int]) -> List[int]:
+        pass
+
+
+if __name__ == '__main__':
+    pass
