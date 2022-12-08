83732fa15809c430f13d10edf41ba5a4246a8dff
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-08 18:31:55 +0900

    Add runningSum.py

---
+++ Data Structures and Algorithms/Arrays and strings/runningSum.py
@@ -0,0 +1,15 @@
+"""
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+Return the running sum of nums.
+"""
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def runningSum(nums: List[int]) -> List[int]:
+        pass
+
+
+if __name__ == '__main__':
+    pass
