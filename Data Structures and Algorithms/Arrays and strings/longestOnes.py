cce4e5cba87c4f2e6afc86f6c32104609abd0c5e
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-08 18:31:40 +0900

    Add longestOnes.py

---
+++ Data Structures and Algorithms/Arrays and strings/longestOnes.py
@@ -0,0 +1,15 @@
+"""
+Given a binary array nums and an integer k,
+return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+"""
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def longestOnes(nums: List[int], k: int) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    pass
