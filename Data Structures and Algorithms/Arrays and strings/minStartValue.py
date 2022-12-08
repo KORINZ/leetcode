b1c950407da06f108cad2b718a56c5b6925741d4
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-08 18:31:48 +0900

    Add minStartValue.py

---
+++ Data Structures and Algorithms/Arrays and strings/minStartValue.py
@@ -0,0 +1,16 @@
+"""
+Given an array of integers nums, you start with an initial positive value startValue.
+In each iteration, you calculate the step by step sum of startValue plus elements in nums (from left to right).
+Return the minimum positive value of startValue such that the step by step sum is never less than 1.
+"""
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def minStartValue(nums: List[int]) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    pass
