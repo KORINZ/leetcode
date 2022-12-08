5135b08c1b88117fcc73968c48f0af032859981c
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-08 18:25:16 +0900

    Finish sortedSquares.py

--- Data Structures and Algorithms/Arrays and strings/sortedSquares.py
+++ Data Structures and Algorithms/Arrays and strings/sortedSquares.py
@@ -2,15 +2,38 @@
 Given an integer array nums sorted in non-decreasing order,
 return an array of the squares of each number sorted in non-decreasing order.
 """
-# Two pointers
 from typing import List
 
 
+# Approach 1: Sort
+# sorted(x * x for x in nums)
+# Time Complexity: O(NlogN), where N is the length of nums.
+# Space complexity : O(N) b.c. Timsort.
+
+
+# Approach 2: Two Pointer
 class Solution:
     @staticmethod
     def sortedSquares(nums: List[int]) -> List[int]:
-        pass
+        n = len(nums)
+        res = [0] * n
+        left, right, square = 0, n - 1, 0
 
+        for i in reversed(range(n)):
+            if abs(nums[left]) > abs(nums[right]):
+                square = nums[left]
+                left += 1
+            else:
+                square = nums[right]
+                right -= 1
+
+            res[i] = square ** 2
+
+        return res
+
+
+# Time Complexity: O(N), where N is the length of A.
+# Space Complexity: O(N)
 
 if __name__ == '__main__':
-    pass
+    print(Solution.sortedSquares(nums=[-4, -1, 0, 3, 10]))  # [0, 1, 9, 16, 100]
