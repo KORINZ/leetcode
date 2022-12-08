be1e9d644f2c11612c5ced5ec7e624938c2d2e8c
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-08 18:31:30 +0900

    Add findMaxAverage.py

---
+++ Data Structures and Algorithms/Arrays and strings/findMaxAverage.py
@@ -0,0 +1,16 @@
+"""
+You are given an integer array nums consisting of n elements, and an integer k.
+Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value.
+Any answer with a calculation error less than 10-5 will be accepted.
+"""
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def findMaxAverage(nums: List[int], k: int) -> float:
+        pass
+
+
+if __name__ == '__main__':
+    pass
