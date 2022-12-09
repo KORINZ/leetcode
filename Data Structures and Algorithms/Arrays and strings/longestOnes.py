71f6e624e8c9da4976d308eabd7ef8d2695b5ef1
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-09 18:05:59 +0900

    Finish longestOnes.py

--- Data Structures and Algorithms/Arrays and strings/longestOnes.py
+++ Data Structures and Algorithms/Arrays and strings/longestOnes.py
@@ -9,8 +9,28 @@ from typing import List
 class Solution:
     @staticmethod
     def longestOnes(nums: List[int], k: int) -> int:
-        pass
+        l = 0
+        counter = 0
+        longest = 0
 
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                counter += 1
+
+            if counter > k:
+                while counter != k:
+                    if nums[l] == 0:
+                        counter -= 1
+                    l += 1
+
+            if longest < r - l + 1:
+                longest = r - l + 1
+
+        return longest
+
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
 
 if __name__ == '__main__':
-    pass
+    print(Solution.longestOnes(nums=[1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], k=2))  # 6
