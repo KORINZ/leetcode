6aeb6621e215bd88e312c8f0f492055a7c53cdef
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-09 12:32:26 +0900

    Add reverseList.py

---
+++ easy/reverseList.py
@@ -0,0 +1,16 @@
+"""
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+"""
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pass
