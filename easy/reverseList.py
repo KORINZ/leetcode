694527c546f461c54d0561c1c70248b3afea3551
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-09 12:46:04 +0900

    Finish reverseList.py

--- easy/reverseList.py
+++ easy/reverseList.py
@@ -1,5 +1,7 @@
 """
 Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
 """
 from typing import Optional
 
@@ -12,5 +14,18 @@ class ListNode:
 
 
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:  # NOQA
+        prev = None
+        curr = head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
+
+    # Time Complexity: O(N)
+    # Space Complexity: O(1)
+
+    # def reverseList_recursive(self):
