d52d871b824d590b1c9964f89516d2373cf0251c
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-09 17:51:46 +0900

    Add output comment

--- Data Structures and Algorithms/Arrays and strings/findMaxAverage.py
+++ Data Structures and Algorithms/Arrays and strings/findMaxAverage.py
@@ -29,4 +29,4 @@ class Solution:
 # Space complexity : O(1)
 
 if __name__ == '__main__':
-    print(Solution.findMaxAverage(nums=[1, 12, -5, -6, 50, 3], k=4))
+    print(Solution.findMaxAverage(nums=[1, 12, -5, -6, 50, 3], k=4))  # 12.75000
