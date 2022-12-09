3ef5f8949f63b1c42521f817c52de45dfe1fe205
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-09 20:42:24 +0900

    Finish fib.py

--- easy/fib.py
+++ easy/fib.py
@@ -9,3 +9,26 @@ F(n) = F(n - 1) + F(n - 2), for n > 1.
 
 Given n, calculate F(n).
 """
+
+
+class Solution:
+    @staticmethod
+    def fib_recursive(n: int) -> int:
+        # Recursive approach
+        # Time complexity : O(2^n)
+        # Space complexity : O(n)
+        if n <= 1:
+            return n
+        return Solution.fib_recursive(n - 1) + Solution.fib_recursive(n - 2)
+
+
+    @staticmethod
+    def fib_iterative(n: int) -> int:
+        # Iterative approach
+        # Time complexity : O(n)
+        # Space complexity : O(1)
+        prev, curr = 0, 1
+        while n > 0:
+            prev, curr = curr, prev + curr
+            n -= 1
+        return prev
