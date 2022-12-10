981a45bc5d874fac6e2bf0b2038adff438e7e10a
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-10 23:17:48 +0900

    Add searchBST.py

---
+++ easy/searchBST.py
@@ -0,0 +1,20 @@
+"""
+https://leetcode.com/problems/search-in-a-binary-search-tree/
+You are given the root of a binary search tree (BST) and an integer val.
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node.
+If such a node does not exist, return null.
+"""
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        pass
