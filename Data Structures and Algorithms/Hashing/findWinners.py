68c40817a47cf36775190daf5df49cec6ebd6a6f
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-13 02:05:04 +0900

    Add output result and explanation

--- Data Structures and Algorithms/Hashing/findWinners.py
+++ Data Structures and Algorithms/Hashing/findWinners.py
@@ -38,3 +38,11 @@ class Solution:
 if __name__ == '__main__':
     print(Solution.findWinners(
         matches=[[1, 3], [2, 3], [3, 6], [5, 6], [5, 7], [4, 5], [4, 8], [4, 9], [10, 4], [10, 9]]))
+
+    # [[1,2,10],[4,5,7,8]]
+
+    # Explanation:
+    # Players 1, 2, and 10 have not lost any matches.
+    # Players 4, 5, 7, and 8 each have lost one match.
+    # Players 3, 6, and 9 each have lost two matches.
+    # Thus, answer[0] = [1,2,10] and answer[1] = [4,5,7,8].
