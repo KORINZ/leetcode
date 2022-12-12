113aaddad0bbd1d284e81532493bdd3a46957cca
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-12 16:58:24 +0900

    Add findWinners.py

---
+++ Data Structures and Algorithms/Hashing/findWinners.py
@@ -0,0 +1,16 @@
+"""
+https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4606/
+You are given an integer array matches where matches[i] = [winner_i, loser_i]
+indicates that the player winner_i defeated player loser_i in a match.
+
+Return a list answer of size 2 where:
+
+answer[0] is a list of all players that have not lost any matches.
+answer[1] is a list of all players that have lost exactly one match.
+The values in the two lists should be returned in increasing order.
+
+Note:
+
+You should only consider the players that have played at least one match.
+The testcases will be generated such that no two matches will have the same outcome.
+"""
