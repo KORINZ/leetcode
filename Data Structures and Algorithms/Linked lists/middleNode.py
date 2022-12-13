c0518a569f2647295076bdae02c08eadf5741c49
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-13 22:49:26 +0900

    Add middleNode.py

---
+++ Data Structures and Algorithms/Linked lists/middleNode.py
@@ -0,0 +1,6 @@
+"""
+https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/704/linked-lists/4660/
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+"""
