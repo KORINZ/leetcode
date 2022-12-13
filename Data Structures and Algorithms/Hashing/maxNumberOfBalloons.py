54188baa983fb133fcb6055bb7c42058d425ce2d
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-13 19:35:28 +0900

    Finish maxNumberOfBalloons.py

--- Data Structures and Algorithms/Hashing/maxNumberOfBalloons.py
+++ Data Structures and Algorithms/Hashing/maxNumberOfBalloons.py
@@ -4,3 +4,19 @@ Given a string text, you want to use the characters of text to form as many inst
 
 You can use each character in text at most once. Return the maximum number of instances that can be formed.
 """
+from collections import Counter
+
+
+class Solution:
+    @staticmethod
+    def maxNumberOfBalloons(text: str) -> int:
+        text_dict = Counter(text)
+
+        return min(text_dict["b"], text_dict["a"], text_dict["l"] // 2, text_dict["o"] // 2, text_dict["n"])
+
+
+# Time complexity : O(n)
+# Space complexity : O(1)
+
+if __name__ == '__main__':
+    print(Solution.maxNumberOfBalloons(text="loonbalxballpoon"))  # 2
