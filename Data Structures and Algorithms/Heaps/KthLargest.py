bdc34d43ca9907722d6f231a53fce92d6bb27020
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-13 22:53:40 +0900

    Add KthLargest.py

---
+++ Data Structures and Algorithms/Heaps/KthLargest.py
@@ -0,0 +1,11 @@
+"""
+https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/708/heaps/4639/
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order,
+not the kth distinct element.
+
+Implement KthLargest class:
+
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+int add(int val) Appends the integer val to the stream and
+returns the element representing the kth largest element in the stream.
+"""
