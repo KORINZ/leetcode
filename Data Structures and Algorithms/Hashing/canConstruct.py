44bdcd24be91b0e1f59c5e5b126642d5bc70eff5
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-14 00:01:37 +0900

    Finish canConstruct.py

--- Data Structures and Algorithms/Hashing/canConstruct.py
+++ Data Structures and Algorithms/Hashing/canConstruct.py
@@ -5,3 +5,24 @@ return true if ransomNote can be constructed by using the letters from magazine 
 
 Each letter in magazine can only be used once in ransomNote.
 """
+from collections import Counter
+
+
+class Solution:
+    @staticmethod
+    def canConstruct(ransom_note: str, magazine: str) -> bool:
+        ransom_note = Counter(ransom_note)
+        magazine = Counter(magazine)
+
+        for key in ransom_note:
+            if ransom_note[key] > magazine[key]:
+                return False
+
+        return True
+
+
+# Time complexity : O(m) where m = len(magazine)
+# Space complexity : O(1)
+
+if __name__ == '__main__':
+    print(Solution.canConstruct(ransom_note="aa", magazine="aab"))  # True
