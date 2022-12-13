84cc92bbaf83242976e408f4059c6e6c57f36085
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-13 22:56:06 +0900

    Add searchInsert.py

---
+++ Data Structures and Algorithms/Binary search/searchInsert.py
@@ -0,0 +1,7 @@
+"""
+https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/710/binary-search/4569/
+Given a sorted array of distinct integers and a target value, return the index if the target is found.
+If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+"""
