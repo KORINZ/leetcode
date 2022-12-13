c75cb6fd6efc3497cb85a22d59476a9ec058ad77
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-13 19:38:08 +0900

    Add numJewelsInStones.py

---
+++ Data Structures and Algorithms/Hashing/numJewelsInStones.py
@@ -0,0 +1,8 @@
+"""
+https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4664/
+You're given strings jewels representing the types of stones that are jewels,
+and stones representing the stones you have.
+Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case-sensitive, so "a" is considered a different type of stone from "A".
+"""
