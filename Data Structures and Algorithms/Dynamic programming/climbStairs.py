2c65506e5b157184dab333f58e732233322e24ba
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-13 22:58:04 +0900

    Add climbStairs.py

---
+++ Data Structures and Algorithms/Dynamic programming/climbStairs.py
@@ -0,0 +1,6 @@
+"""
+https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/712/dynamic-programming/4580/
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+"""
