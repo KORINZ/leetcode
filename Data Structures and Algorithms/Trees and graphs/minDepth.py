a9a12ea19cb1da3444b93e21b37a5f87d1c60799
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-13 22:51:59 +0900

    Add minDepth.py

---
+++ Data Structures and Algorithms/Trees and graphs/minDepth.py
@@ -0,0 +1,8 @@
+"""
+https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/707/traversals-trees-graphs/4691/
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+"""
