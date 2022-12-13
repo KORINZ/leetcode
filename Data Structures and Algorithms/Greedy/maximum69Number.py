9423b03a1ee643b7652f6a3f63e39962774d70cd
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-13 22:54:59 +0900

    Add maximum69Number.py

---
+++ Data Structures and Algorithms/Greedy/maximum69Number.py
@@ -0,0 +1,6 @@
+"""
+https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/709/greedy/4560/
+You are given a positive integer num consisting only of digits 6 and 9.
+
+Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+"""
