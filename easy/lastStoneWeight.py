d78c4509998d58e463741115c2a70f651b275211
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-15 00:36:26 +0900

    Finish lastStoneWeight.py

--- easy/lastStoneWeight.py
+++ easy/lastStoneWeight.py
@@ -11,3 +11,32 @@ At the end of the game, there is at most one stone left.
 
 Return the weight of the last remaining stone. If there are no stones left, return 0.
 """
+from typing import List
+import heapq
+
+
+class Solution:
+    @staticmethod
+    def lastStoneWeight(stones: List[int]) -> int:
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            first = heapq.heappop(stones)
+            second = heapq.heappop(stones)
+            if second > first:
+                heapq.heappush(stones, first - second)
+
+        return abs(stones[0]) if stones else 0
+
+
+# Time Complexity: O(n*log(n))
+# Memory Complexity: O(1)
+
+if __name__ == '__main__':
+    print(Solution.lastStoneWeight(stones=[2, 7, 4, 1, 8, 1]))  # 1
+    # Explanation:
+    # We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+    # we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+    # we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+    # we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.
