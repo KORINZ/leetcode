f4ca42328bc4193c52c2c6c663cfced330b476d4
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-16 15:35:54 +0900

    Finish topKFrequent.py

--- medium/topKFrequent.py
+++ medium/topKFrequent.py
@@ -4,12 +4,19 @@ Given an integer array nums and an integer k, return the k most frequent element
 You may return the answer in any order.
 """
 from typing import List
+from collections import Counter
 
 
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        pass
+    @staticmethod
+    def topKFrequent(nums: List[int], k: int) -> List[int]:
+        nums = Counter(nums)
+
+        return [i[0] for i in nums.most_common(k)]
+
+# Time Complexity: O(Nlog(k))
+# Space Complexity: O(N+k)
 
 
 if __name__ == '__main__':
-    pass
+    print(Solution.topKFrequent(nums=[1, 1, 1, 2, 2, 3], k=2))  # [1, 2]
