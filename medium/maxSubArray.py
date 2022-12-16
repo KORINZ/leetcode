62da6306d875b44b8dee417bd9badacbb3fc66d9
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-16 23:01:55 +0900

    Add maxSubArray.py

---
+++ medium/maxSubArray.py
@@ -0,0 +1,14 @@
+"""
+https://leetcode.com/problems/maximum-subarray/
+Given an integer array nums, find the subarray which has the largest sum and return its sum.
+"""
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    pass
