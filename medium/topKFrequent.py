ddab33d013f50243aef74b9bf96be53a9707d92f
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-16 15:29:06 +0900

    Add topKFrequent.py

---
+++ medium/topKFrequent.py
@@ -0,0 +1,15 @@
+"""
+https://leetcode.com/problems/top-k-frequent-elements/
+Given an integer array nums and an integer k, return the k most frequent elements.
+You may return the answer in any order.
+"""
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        pass
+
+
+if __name__ == '__main__':
+    pass
