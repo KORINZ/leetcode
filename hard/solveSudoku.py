378aad1e6dede2cb62318d6c056f83cb0fe765f0
KORINZ <111611023+KORINZ@users.noreply.github.com>
2023-03-09 01:00:13 +0900

    Add solveSudoku.py file

---
+++ hard/solveSudoku.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        pass
+
+
+if __name__ == '__main__':
+    pass
